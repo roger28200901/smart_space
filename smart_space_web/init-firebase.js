4826431464311be889175bba162db723839ceee9
YunHong <roger28200901@gmail.com>
2021-10-08 14:34:13 +0800

    Init all files

---
+++ smart_space_web/init-firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDU3TzhMAvrGtVDYjWFocC2JF3kgI6RzC8",
+    authDomain: "smart-space-c3a9d.firebaseapp.com",
+    databaseURL: "https://smart-space-c3a9d-default-rtdb.firebaseio.com",
+    projectId: "smart-space-c3a9d",
+    storageBucket: "smart-space-c3a9d.appspot.com",
+    messagingSenderId: "1066225934091",
+    appId: "1:1066225934091:web:c4c32640ce9721fcd44c0e",
+    measurementId: "G-PLJRTXENNB"
+};
+firebase.initializeApp(firebaseConfig);
